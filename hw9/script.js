d1b451d364a1b4480e567097af307a2f09ec6e54
alexhrodz <alex.hrodz@gmail.com>
2019-01-21 23:44:06 +0200

    fixed script.js

--- hw9/script.js
+++ hw9/script.js
@@ -1,14 +1,14 @@
 let a = 11;
 
 if (a > 10) {
-	console.log("This number is more than 10");
+  console.log("This number is more than 10");
 } else {
-	console.log("This number is less than 10");
+  console.log("This number is less than 10");
 }
 
 let lang = "ru",
-	arrRu = ["Октябрь", "Ноябрь", "Декабрь","Январь", "Февраль", "Март","Апрель", "Май", "Июнь","Июль", "Август", "Сентябрь"],
-	arrEn = ["October", "November", "December","January", "February", "March","April", "May", "June","July", "August", "September"];
+    arrRu = ["Октябрь", "Ноябрь", "Декабрь","Январь", "Февраль", "Март","Апрель", "Май", "Июнь","Июль", "Август", "Сентябрь"],
+    arrEn = ["October", "November", "December","January", "February", "March","April", "May", "June","July", "August", "September"];
 
 switch (lang) {
   case "ru":
@@ -24,9 +24,9 @@ switch (lang) {
 let arr = [2, 1, 0, 4, 6, 7, 13, 222, 5, 2.2], i;
 
 for (i = 0; i < arr.length; i++) { 
-	if ((arr[i] > 1) && !(arr[i] & 1)) {
-		console.log(arr[i]);
-	}
+  if ((arr[i] > 1) && !(arr[i] & 1)) {
+    console.log(arr[i]);
+  }
 }
 
 let j = 1;
